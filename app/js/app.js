5c247c8d3e948f8dd519e49a153660754bf05f12
Richard Osterloh <richard.osterloh@gmail.com>
2015-03-02 14:32:23 +0000

    remove unneeded files

--- app/js/app.js
+++
@@ -1,7 +0,0 @@
-angular.module('NoteWrangler', ['ngRoute'])
-	.config(function($routeProvider) {
-    $routeProvider
-    	.when('/notes', {
-    		templateUrl: 'templates/pages/notes/index.html'
-    	});
-  });
